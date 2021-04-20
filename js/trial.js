f4fe5380f5755d5b4c233ee6bbd666ff584c75a7
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-04-20 14:25:58 +0200

    string tokenizer

---
+++ js/trial.js
@@ -0,0 +1,9 @@
+debugger;
+
+let hello = "world1";
+console.assert(hello === "world", 'hello should be "world"');
+
+hello = "good bye2";
+console.assert(hello === "good bye", 'hello should be "good bye"');
+
+console.log("world");
