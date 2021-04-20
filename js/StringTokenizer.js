0be4fdd819fc96978651ca807d48a54b286b4542
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-04-20 15:34:53 +0200

    String tokenizer

--- js/StringTokenizer.js
+++ js/StringTokenizer.js
@@ -11,14 +11,45 @@ function stringTokenizer(textToTokenize){
     //token to words
     for (let i = 0; i < returnArray.length; i++) {
         //get from front (shift) add to back (push)
-        const element = returnArray[i];
-        
+        // const element = returnArray.shift();
+        returnArray.push(getWordTokens(returnArray.shift()));        
     }
 
     return returnArray;
 }
 
 function getLineTokens(textToTokenize){
+    let returnArray = [];
+    //get from front (shift) add to back (push)
+
+    for(let i = 0; i < textToTokenize.length; i++){
+        let char = textToTokenize[i];
+        if(char !== '\n')
+            returnArray.push(char);
+
+    }
+
+    return returnArray;
+}
+
+function getWordTokens(textLineToTokenize){
     //get from front (shift) add to back (push)
 
 }
+
+let stringVar = 'The \nend !!!';
+
+console.log('The \nend !!!');
+console.log(stringVar.charCodeAt(4));
+
+for (let i = 0; i < stringVar.length; i++) {
+    const element = stringVar[i];
+    console.log(i, element, ' ', element.charCodeAt());
+    
+}
+
+
+// for (let i=0; i < 200; i++){
+//     let s = String.fromCharCode(i);
+//     console.log(i, s);
+//   }
