15e2420ea76f9d3c3ad8ebfe5844ede71e8fd0c0
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-04-25 12:55:52 +0200

    JS same but in function

--- js/ArrayComparison.js
+++ js/ArrayComparison.js
@@ -24,3 +24,20 @@ const deepCompareArrays = (actual, expect) => {
           ))))
   );
 };
+//same with function definition
+function deepCompare(actual, expect) {
+  return (
+    actual === expect ||
+    Object.is(actual, expect) ||
+    (Object(actual) === actual &&
+      Object(expect) === expect &&
+      ((Array.isArray(actual) &&
+        Array.isArray(expect) &&
+        actual.length === expect.length &&
+        expect.every((expect, index) => deepCompare(actual[index], expect))) ||
+        (Object.keys(actual).length === Object.keys(expect).length &&
+          Object.keys(expect).every((key) =>
+            deepCompare(actual[key], expect[key])
+          ))))
+  );
+}
