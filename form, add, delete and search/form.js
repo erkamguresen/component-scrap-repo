434700e3372151eaa1e4e4aa537edd4e4a9c5a9c
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-02 12:11:54 +0200

    update

--- form, add, delete and search/form.js
+++ form, add, delete and search/form.js
@@ -5,11 +5,50 @@ let itemList = document.getElementById("items");
 //Form Submit Event
 form.addEventListener("submit", addItem);
 
+//delete event
+itemList.addEventListener("click", removeItem);
+
 //Add Item
 function addItem(e) {
   e.preventDefault();
 
   //Get Input value
-  let newItem = e.target.value;
-  console.log(e);
+  let newItem = document.getElementById("addItem");
+
+  //get text value
+  let newItemValue = newItem.value;
+
+  //Create new li Element
+  let newLi = document.createElement("li");
+
+  //Add Class to Li
+  newLi.className = "list-group-item";
+
+  //Add text node with input value
+  newLi.appendChild(document.createTextNode(newItemValue));
+
+  //create delete Button
+  let deleteButton = document.createElement("button");
+
+  deleteButton.className = "btn btn-danger btn-sm float-right delete";
+  deleteButton.textContent = "X";
+
+  newLi.appendChild(deleteButton);
+
+  itemList.appendChild(newLi);
+}
+
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    let parentNode = e.target.parentElement;
+
+    let isConfirmed = confirm(
+      //TODO: there is formatting and extra X
+      "Are you sure you want to delete " + parentNode.textContent + " ?"
+    );
+
+    if (isConfirmed) {
+      //TODO: 13.27
+    }
+  }
 }
