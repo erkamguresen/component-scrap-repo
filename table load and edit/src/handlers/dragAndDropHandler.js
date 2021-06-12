6b50c527039d5acd34049ff446b0e01f1e0fba2d
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-06-12 22:41:20 +0200

    excel trials

---
+++ table load and edit/src/handlers/dragAndDropHandler.js
@@ -0,0 +1,26 @@
+import { uploadFile } from '../procedures/upload-files.js';
+
+export function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+export function highlight(e) {
+  dropArea.classList.add('highlight');
+}
+
+export function unhighlight(e) {
+  dropArea.classList.remove('highlight');
+}
+
+export function handleDrop(e) {
+  let dt = e.dataTransfer;
+  let files = dt.files;
+
+  [...files].forEach(uploadFile);
+}
+
+export function handleFiles(event) {
+  const files = event.currentTarget.files;
+  [...files].forEach(uploadFile);
+}
---
+++ table load and edit/src/procedures/upload-files.js
@@ -0,0 +1,25 @@
+import { getFileName } from './IO-file.js';
+// import { XLSX } from '//unpkg.com/xlsx/dist/xlsx.full.min.js';
+
+export function uploadFile(file) {
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
+
+    /* DO SOMETHING WITH workbook HERE */
+
+    console.log(workbook);
+
+    const sheetName = workbook.SheetNames[0];
+    var sheet = workbook.Sheets[sheetName];
+    document.getElementById('data-table').innerHTML =
+      XLSX.utils.sheet_to_html(sheet);
+  };
+
+  reader.readAsArrayBuffer(file);
+
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
+}
