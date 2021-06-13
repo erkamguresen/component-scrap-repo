8a07713d7f47aef9717c8f0a4c812b77c0013f3e
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-06-13 12:14:57 +0200

    some notes

--- table load and edit/src/procedures/xlsxToCustomTable.js
+++ table load and edit/src/procedures/xlsxToCustomTable.js
@@ -1,13 +1,11 @@
 //https://docs.sheetjs.com/
 
-export function (){
+// TODO: will this import work
+//import { XLSX } from 'https://unpkg.com/xlsx@0.17.0/dist/xlsx.full.min.js';
 
-    //use w values to make table data
-
-    //first row is header uneditable
-    // first elements in first row is empty uneditable
-
-    //first element of each row is number
-
-
+export function xlsxToCustomTable() {
+  //use w values to make table data
+  //first row is header uneditable
+  // first elements in first row is empty uneditable
+  //first element of each row is number
 }
